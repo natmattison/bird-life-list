c1500f849b69f3cd417c1ed834684c7dd0f137fa
natmat <natmattison@gmail.com>
2017-09-18 21:27:47 -0400

    wip sightings index

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -2,6 +2,7 @@ class SightingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @bird_count = current_user.sightings.select(:bird_id).distinct.count
   end
   
   def new
